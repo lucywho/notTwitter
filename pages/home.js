6186c12388ef719378f016ef6ebb6f99e0ab91b8
Lucy Toman <lucy@toman.me.uk>
2022-06-01 10:25:46 +0200

    sets up NextAuth and Prisma

---
+++ pages/home.js
@@ -0,0 +1,11 @@
+import { useSession } from "next-auth/react"
+import NewTweet from "components/NewTweet"
+
+export default function Home() {
+    const { data: session, status } = useSession()
+
+    if (status === "loading") {
+        return <p>...loading</p>
+    }
+    return <div>{session ? <NewTweet /> : <p>You are not logged in 😞</p>}</div>
+}
