41a30bd5ebc18e467ffc38dafd8432420cad5c0b
Lucy Toman <lucy@toman.me.uk>
2022-06-02 19:08:09 +0200

    adds fake data

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,6 +1,10 @@
 import timeago from "lib/timeago"
 
 export default function Tweet({ tweet }) {
+    if (!tweet.author.name) {
+        tweet.author.name = "anonymous"
+    }
+
     return (
         <p>
             {timeago.format(new Date(tweet.createdAt))} {": "}
