30e97497dd346d2d4f984637196ca1d4ed272df1
Lucy Toman <lucy@toman.me.uk>
2022-06-28 12:27:42 +0200

    updates nextauth switch between dev and production

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,20 +3,29 @@ import EmailProvider from "next-auth/providers/email"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import prisma from "lib/prisma"
 
-export default NextAuth({
-    providers: [
-        EmailProvider({
-            server: {
-                host: process.env.EMAIL_SERVER_HOST,
-                port: process.env.EMAIL_SERVER_PORT,
-                auth: {
-                    user: process.env.EMAIL_SERVER_USER,
-                    pass: process.env.EMAIL_SERVER_PASSWORD,
-                },
+let data
+
+if (process.env.NODE_ENV === "development") {
+    data = {
+        server: process.env.EMAIL_SERVER,
+        from: process.env.EMAIL_FROM,
+    }
+} else {
+    data = {
+        server: {
+            host: process.env.EMAIL_SERVER_HOST,
+            port: process.env.EMAIL_SERVER_PORT,
+            auth: {
+                user: process.env.EMAIL_SERVER_USER,
+                pass: process.env.EMAIL_SERVER_PASSWORD,
             },
-            from: process.env.EMAIL_FROM,
-        }),
-    ],
+        },
+        from: process.env.EMAIL_FROM,
+    }
+}
+
+export default NextAuth({
+    providers: [EmailProvider(data)],
 
     database: process.env.DATABASE_URL,
     secret: process.env.SECRET,
@@ -28,24 +37,14 @@ export default NextAuth({
 
     debug: true,
     adapter: PrismaAdapter(prisma),
+
+    callbacks: {
+        session: async ({ session, user }) => {
+            session.user.id = user.id
+            session.user.image = user.image
+            session.user.username = user.username
+            session.user.name = user.name
+            return Promise.resolve(session)
+        },
+    },
 })
-
-// export default NextAuth({
-//     providers: [
-//         EmailProvider({
-//             server: process.env.EMAIL_SERVER,
-//             from: process.env.EMAIL_FROM,
-//         }),
-//     ],
-
-//     database: process.env.DATABASE_URL,
-//     secret: process.env.SECRET,
-
-//     session: {
-//         jwt: true,
-//         maxAge: 30 * 24 * 60 * 60, //30 days
-//     },
-
-//     debug: true,
-//     adapter: PrismaAdapter(prisma),
-// })
